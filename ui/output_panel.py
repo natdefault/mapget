ec94d4c45dc457241bae6f6d49274584ace23cac
natdefault <loshermanostocino@hotmail.com>
2026-05-22 22:14:45 -0300

    h

---
+++ ui/output_panel.py
@@ -0,0 +1,9 @@
+﻿from PySide6.QtWidgets import QWidget, QVBoxLayout, QTextEdit
+
+
+class OutputPanel(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        layout = QVBoxLayout()
+        layout.addWidget(QTextEdit())
+        self.setLayout(layout)
